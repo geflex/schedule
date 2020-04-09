35129679d838b66498c1fb0f4cd984d845d7353a
geflex <geflikh.d.s@gmail.com>
2020-04-09 18:22:00 +0300

    Fix for prev commit: deleted some modules

--- schedule/bottex/typing.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable, Optional
-
-from bottex.messaging import Request
-from bottex.messaging.responses import Message
-
-
-Receiver = Callable[[Request], Optional[Message]]
-ErrorHandler = Callable[[Request, Exception], Message]
-Match = Callable[[Request], bool]
