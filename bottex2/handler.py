9bb6b3477da556faba9403a89fb7e9611392789b
geflex <geflikh.d.s@gmail.com>
2020-10-20 20:12:15 +0300

    Params now supports dot access to keys.

--- bottex2/handler.py
+++ bottex2/handler.py
@@ -1,14 +1,16 @@
 import inspect
-from typing import Awaitable, TypedDict, Any, Callable
+from typing import Awaitable, Any, Callable
 
 from bottex2 import tools
 from bottex2.chat import Chat
 
 
-class Params(TypedDict):
-    text: str
-    chat: Chat
-    raw: Any
+class Params(dict):
+    def __init__(self, *, text: str, chat: Chat, raw: Any):
+        super().__init__(text=text, chat=chat, raw=raw)
+
+    def __getattr__(self, attr):
+        return self[attr]
 
 
 Handler = Callable[..., Awaitable]
