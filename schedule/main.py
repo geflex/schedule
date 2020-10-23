e8bf8dacc51a4cfbc2b40e6262922730eeb6cdd4
geflex <geflikh.d.s@gmail.com>
2020-10-23 23:53:38 +0300

    Add SQLAlchemy support to `schedule` app.

--- schedule/main.py
+++ schedule/main.py
@@ -2,12 +2,14 @@ import sys; sys.path.extend(['D:\\Documents\\Code\\Python\\schedule'])
 import logging
 
 import motor.motor_asyncio
+from sqlalchemy import create_engine
 
 from bottex2.platforms.tg import TgReceiver
 from bottex2.platforms.vk import VkReceiver
 
 from bottex2.middlewares import loggers, users
 from bottex2.databases.mongodb import MongoUser
+from bottex2.databases.sqlalchemy import SqlAalchemyUser, set_engine, Base
 from bottex2.bottex import Bottex
 
 
@@ -24,6 +26,14 @@ def set_mongo_user_model():
     MongoUser.set_collection(mongo.schedule_test.users)
 
 
+def set_sql_user_model():
+    addr = 'geflex.mysql.pythonanywhere-services.com'
+    engine = create_engine('sqlite:///./foo.db')  # pool_recycle=280)
+    set_engine(engine)
+    users.set_user_model(SqlAalchemyUser)
+    Base.metadata.create_all(engine)
+
+
 def set_middlewares():
     bottex.add_middleware(users.UserBottexHandlerMiddleware)
     bottex.add_middleware(loggers.BottexLoggingHandlerMiddleware)
@@ -32,6 +42,6 @@ def set_middlewares():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    set_mongo_user_model()
+    set_sql_user_model()
     set_middlewares()
     bottex.serve_forever()
