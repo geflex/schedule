c94ec2464e774c9b8b0a687a579a684e223766f0
geflex <geflikh.d.s@gmail.com>
2020-10-30 11:37:15 +0300

    Legacy code commented

--- schedule/bases.py
+++ schedule/bases.py
@@ -1,6 +1,6 @@
 import datetime
 
-from bottex2.legacy import regexp as re
+from bottex2 import regexp as re
 
 group_fmt = re.compile(r'\d{8}')
 time_fmt = re.compile(r'(?P<hour>[01]?\d|2[0-3])[:.\-]?(?P<minute>[0-5]\d)')
@@ -12,21 +12,21 @@ def str_time(t):
     return ''
 
 
-class BackButton(ButtonLink):
-    def __init__(self, callback, *, next_line=True):
-        super().__init__('Назад', callback, 'Переходим назад', color=Color.WHITE, next_line=next_line)
-
-
-class CancelButton(ButtonLink):
-    def __init__(self, callback, *, next_line=True):
-        super().__init__('Отмена', callback, 'Отменяем', color=Color.RED, next_line=next_line)
-
-
-class NotChangeButton(ButtonLink):
-    def __init__(self, callback, *, next_line=True):
-        super().__init__('Не менять', callback, 'Не меняем', color=Color.WHITE, next_line=next_line)
-
-
-class PassButton(ButtonLink):
-    def __init__(self, callback, *, next_line=True):
-        super().__init__('Пропустить', callback, 'Пропускаем', color=Color.WHITE, next_line=next_line)
+# class BackButton(ButtonLink):
+#     def __init__(self, callback, *, next_line=True):
+#         super().__init__('Назад', callback, 'Переходим назад', color=Color.WHITE, next_line=next_line)
+#
+#
+# class CancelButton(ButtonLink):
+#     def __init__(self, callback, *, next_line=True):
+#         super().__init__('Отмена', callback, 'Отменяем', color=Color.RED, next_line=next_line)
+#
+#
+# class NotChangeButton(ButtonLink):
+#     def __init__(self, callback, *, next_line=True):
+#         super().__init__('Не менять', callback, 'Не меняем', color=Color.WHITE, next_line=next_line)
+#
+#
+# class PassButton(ButtonLink):
+#     def __init__(self, callback, *, next_line=True):
+#         super().__init__('Пропустить', callback, 'Пропускаем', color=Color.WHITE, next_line=next_line)
