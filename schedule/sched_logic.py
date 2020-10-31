1d7405faac215193c95f14c9c39b9120182b4139
geflex <geflikh.d.s@gmail.com>
2020-10-31 09:43:18 +0300

    Added dynamic routing in `schedule.settings`

--- schedule/sched_logic.py
+++ schedule/sched_logic.py
@@ -5,6 +5,7 @@ from bottex2.handler import Request
 from schedule.models import PType
 from schedule._logic import Date
 
+
 schedule_kb = Keyboard([
     [Button('Сегодня')],
     [Button('Завтра')],
@@ -22,15 +23,27 @@ def get_settings_kb(r: Request):
     return keyboard
 
 
+async def become_teacher(r: Request):
+    await r.user.update(ptype=PType.teacher)
+    await r.chat.send_message('Теперь ты препод', get_settings_kb(r))
+
+
+async def become_student(r: Request):
+    await r.user.update(ptype=PType.student)
+    await r.chat.send_message('Теперь ты студент', get_settings_kb(r))
+
+
+async def settings(r: Request):
+    router = Router(default=unknown_settings_command, name='settings')
+    if r.user.ptype is PType.teacher:
+        router.add_route(text_cond('Стать студентом'), become_student)
+    else:
+        router.add_route(text_cond('Стать преподом'), become_teacher)
+    router.add_route(text_cond('Назад'), switch_to_schedule)
+    return await router(r)
+
+
 unknown_command_str = 'Хм непонятная команда'
-
-
-async def unknown_command(r: Request):
-    await r.chat.send_message(unknown_command_str, schedule_kb)
-
-
-async def unknown_settings_command(r: Request):
-    await r.chat.send_message(unknown_command_str, get_settings_kb(r))
 
 
 async def today(r: Request):
@@ -41,14 +54,13 @@ async def tomorrow(r: Request):
     await r.chat.send_message(f'Так расписание для {r.user.group} на {Date.tomorrow()}', schedule_kb)
 
 
-async def back(r: Request):
+async def switch_to_schedule(r: Request):
     await r.chat.send_message('Главное меню', schedule_kb)
     await r.user.update(state=schedule.__name__)
 
 
-settings = Router({
-    text_cond('назад'): back,
-}, default=unknown_settings_command, name='settings')
+async def unknown_settings_command(r: Request):
+    await r.chat.send_message(unknown_command_str, get_settings_kb(r))
 
 
 async def switch_to_settings(r: Request):
@@ -56,8 +68,12 @@ async def switch_to_settings(r: Request):
     await r.user.update(state=settings.__name__)
 
 
+async def unknown_schedule_command(r: Request):
+    await r.chat.send_message(unknown_command_str, schedule_kb)
+
+
 schedule = Router({
     text_cond('сегодня'): today,
     text_cond('завтра'): tomorrow,
     text_cond('настройки'): switch_to_settings,
-}, default=unknown_command, name='schedule')
+}, default=unknown_schedule_command, name='schedule')
