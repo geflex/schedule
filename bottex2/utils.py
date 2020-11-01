fcb87b361be4f91af68394fc8a31b66ab2442b4d
geflex <geflikh.d.s@gmail.com>
2020-11-01 03:43:08 +0300

    Deleted unnecessary line in `gen_state_conds`

--- bottex2/utils.py
+++ bottex2/utils.py
@@ -9,7 +9,6 @@ def gen_state_conds(handlers: List[Union[Type[View], FunctionType]]):
     routes = {}
     for handler in handlers:
         if isinstance(handler, type) and issubclass(handler, View):
-            routes[state_cond(handler.name)] = handler.handle
             state, handler = handler.name, handler.handle
         else:
             state = handler.__name__
