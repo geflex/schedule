a24805eb4f3b373a6b89d1c8fa10a38637f92e7c
geflex <geflikh.d.s@gmail.com>
2020-11-01 03:35:25 +0300

    Changed type hints in `gen_state_conds`

--- bottex2/utils.py
+++ bottex2/utils.py
@@ -1,11 +1,11 @@
 from types import FunctionType
-from typing import List
+from typing import List, Union, Type
 
 from bottex2.middlewares.users import state_cond
 from bottex2.views import View
 
 
-def gen_state_conds(handlers: List[FunctionType]):
+def gen_state_conds(handlers: List[Union[Type[View], FunctionType]]):
     routes = {}
     for handler in handlers:
         if isinstance(handler, type) and issubclass(handler, View):
