df967b15792c11b172fb36539517450e8d5c812c
geflex <geflikh.d.s@gmail.com>
2020-11-01 20:08:22 +0300

    New config variable `time_started`

--- schedule/configs/__init__.py
+++ schedule/configs/__init__.py
@@ -1,6 +1,8 @@
 from . import tg, vk
 import os
+import datetime
 
 
+time_started = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3)))
 host = os.environ.get('HOST', None)
 db_url = os.environ.get('DATABASE_URL')
