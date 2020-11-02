3602ec9855334265d89457fbf38bf1778020dc59
geflex <geflikh.d.s@gmail.com>
2020-11-02 22:50:46 +0300

    Added subgroup support at registration

--- schedule/logic.py
+++ schedule/logic.py
@@ -34,28 +34,42 @@ class PTypeInput(View):
 
     @cached_property
     def commands(self) -> List[List[Command]]:
-        return [
-            [
+        return [[
                 Command('Студент', student_ptype_input),
-                Command('Препод', teacher_ptype_input)
-            ]
-        ]
+                Command('Препод', teacher_ptype_input),
+        ]]
 
     async def default(self, r: Request):
         await r.chat.send_message('Непонятный тип профиля', self.keyboard)
 
 
 async def start_group_input(r: Request):
-    await r.user.update(group=r.text, state=sched_logic.Schedule.name)
-    await success_registration(r)
+    await r.user.update(group=r.text)
+    await StartSubgroupInput.switch(r)
+
+
+class StartSubgroupInput(sched_logic.SettingsSubgroup):
+    name = 'start_subgroup_input'
+
+    def get_subgroup_setter(self, subgroup_num: str):
+        async def subgroup_setter(r: Request):
+            old_subgroup = r.user.subgroup
+            await r.user.update(subgroup=subgroup_num, state=sched_logic.Schedule.name)
+            await send_end_registratioon_message(r)
+        return subgroup_setter
+
+    @classmethod
+    async def switch(cls, r: Request):
+        await r.chat.send_message('Выбери свою подгруппу', cls(r).keyboard)
+        await super(sched_logic.SettingsSubgroup, cls).switch(r)
 
 
 async def start_name_input(r: Request):
     await r.user.update(name=r.text, state=sched_logic.Schedule.name)
-    await success_registration(r)
+    await send_end_registratioon_message(r)
 
 
-async def success_registration(r: Request):
+async def send_end_registratioon_message(r: Request):
     await r.chat.send_message('Ура, все настроили', sched_logic.Schedule(r).keyboard)
 
 
@@ -67,6 +81,7 @@ async def delete_me(r: Request):
 conds = gen_state_conds([
         PTypeInput,
         start_group_input,
+        StartSubgroupInput,
         start_name_input,
         sched_logic.Schedule,
         sched_logic.Settings,
