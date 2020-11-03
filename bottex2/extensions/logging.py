bf47a0c1d6c39f901e60266937d4248042b1ed03
geflex <geflikh.d.s@gmail.com>
2020-11-04 02:24:10 +0300

    Changed messages in `extensions.logging`

--- bottex2/extensions/logging.py
+++ bottex2/extensions/logging.py
@@ -9,7 +9,7 @@ class BottexLoggingHandlerMiddleware(BottexHandlerMiddleware):
     __universal__ = True
 
     async def __call__(self, request):
-        logger.info(f'received {request.text!r}')
+        logger.info(f'in: {request.text!r}')
         await self.handler(request)
 
 
@@ -17,5 +17,5 @@ class BottexLoggingChatMiddleware(BottexChatMiddleware):
     __universal__ = True
 
     async def send_message(self, text: Optional[str] = None, kb: Optional[Keyboard] = None):
-        logger.info(f'sending {text!r}')
+        logger.info(f'out: {text!r}')
         await self.chat.send_message(text, kb)
