7977da265bdb8163f4f105d3f026e252dab826f1
geflex <geflikh.d.s@gmail.com>
2020-11-06 00:49:59 +0300

    Added translation for incoming messages

--- bottex2/ext/i18n.py
+++ bottex2/ext/i18n.py
@@ -1,11 +1,11 @@
 import gettext
 from enum import Enum
-from typing import List, Optional, Dict
+from typing import Optional
 
 from sqlalchemy import Column, types as sqltypes
 
-from bottex2.chat import Keyboard
 from bottex2.bottex import BottexChatMiddleware, BottexHandlerMiddleware
+from bottex2.chat import Keyboard
 from bottex2.handler import Request
 
 
@@ -19,17 +19,20 @@ class I18nUserMixin:
     locale = Column(sqltypes.Enum(Lang))
 
 
+def translate(text, lang):
+    try:
+        trans = gettext.translation('schedule', 'schedule/locales', [lang])
+    except FileNotFoundError:
+        pass
+    else:
+        return trans.gettext(text)
+
+
 class TranslateBottexChatMiddleware(BottexChatMiddleware):
     __universal__ = True
 
     async def send_message(self, text: Optional[str] = None, kb: Optional[Keyboard] = None):
-        locale = self.lang or Lang.ru
-        try:
-            trans = gettext.translation('schedule', 'schedule/locales', [locale.value])
-        except FileNotFoundError:
-            pass
-        else:
-            text = trans.gettext(text)
+        text = translate(text, self.lang.value)
         await super().send_message(text, kb)
 
 
@@ -37,5 +40,7 @@ class TranslateBottexHandlerMiddleware(BottexHandlerMiddleware):
     __universal__ = True
 
     async def __call__(self, request: Request):
-        request.chat.lang = request.user.locale
+        lang = request.user.locale or Lang.ru
+        request.chat.lang = lang
+        request.text = translate(request.text, lang.value)
         await super().__call__(request)
