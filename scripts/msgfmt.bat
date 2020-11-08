ea285ec207516c10d31f583cc736a62b8cc600ba
geflex <geflikh.d.s@gmail.com>
2020-11-08 21:13:49 +0300

    Added generation reversed.mo

--- scripts/msgfmt.bat
+++ scripts/msgfmt.bat
@@ -2,3 +2,4 @@
 call scripts/_setup_i18n.bat
 python %PY_I18N_PATH%/msgfmt.py -o %EN_LOCALES_PATH%/schedule.mo %EN_LOCALES_PATH%/schedule
 python %PY_I18N_PATH%/msgfmt.py -o %EN_LOCALES_PATH%/reversible.mo %EN_LOCALES_PATH%/reversible
+python %PY_I18N_PATH%/msgfmt.py -o %EN_LOCALES_PATH%/reversed.mo %EN_LOCALES_PATH%/reversed
