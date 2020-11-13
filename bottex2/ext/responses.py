b41277d6d201ecba106ca9c48b6e65b4bea621aa
geflex <geflikh.d.s@gmail.com>
2020-11-13 22:48:35 +0300

    Removed `LazyChatMiddleware` because of deprecation of `ChatMiddleware`

--- bottex2/ext/responses.py
+++ bottex2/ext/responses.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Optional, Iterable
 
 from bottex2.bottex import BottexMiddleware
-from bottex2.chat import AbstractChat, Keyboard, ChatMiddleware
+from bottex2.chat import Keyboard
 from bottex2.handler import Request
 
 
@@ -15,19 +15,6 @@ class Message:
         yield self
 
 
-class LazyChatMiddleware(ChatMiddleware):
-    def __init__(self, chat: AbstractChat):
-        super().__init__(chat)
-        self.responses = []
-
-    async def send_message(self, text: Optional[str] = None, kb: Optional[Keyboard] = None):
-        self.responses.append(Message(text=text, kb=kb))
-
-    async def flush(self):
-        for response in self.responses:
-            await super().send_message(text=response.text, kb=response.kb)
-
-
 def response_factory(text: Optional[str] = None, kb: Optional[Keyboard] = None):
     return Message(text, kb)
 
@@ -36,13 +23,8 @@ class ResponseBottexMiddleware(BottexMiddleware):
     __unified__ = True
 
     async def __call__(self, request: Request):
-        chat = LazyChatMiddleware(request.chat)
-        request.chat = chat
         request.resp = response_factory
         response = await super().__call__(request)
-        send = super(LazyChatMiddleware, chat).send_message
-        if response is None:
-            response = chat.responses
         if isinstance(response, Iterable):
             for resp in response:
-                await send(resp.text, resp.kb)
+                await request.chat.send_message(resp.text, resp.kb)
