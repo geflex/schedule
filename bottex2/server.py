edb4273d6452a08c7d57c3e7a228e0844005fb69
geflex <geflikh.d.s@gmail.com>
2020-12-22 18:44:41 +0300

    Removed `AbstractServer` class

--- bottex2/server.py
+++ bottex2/server.py
@@ -1,26 +1,28 @@
 from abc import ABC, abstractmethod
-from typing import AsyncIterator, Iterable, Awaitable
+from typing import AsyncIterator, Iterable
 
-from bottex2.handler import Request, ErrorResponse, Handler, TResponse, Response
+from bottex2.handler import Request, ErrorResponse, Handler, Response
 from bottex2.helpers import aiotools
 
 
-class AbstractServer(ABC):
+class Transport(ABC):
     @abstractmethod
     async def listen(self) -> AsyncIterator[Request]:
         yield
 
     @abstractmethod
-    async def _handle(self, request: Request) -> TResponse:
-        pass
-
-    @abstractmethod
     async def send(self, request: Request, response: Response):
         pass
 
+
+class Server(ABC):
+    def __init__(self, transport: Transport, handler: Handler):
+        self._transport = transport
+        self._handler = handler
+
     async def handle(self, request: Request):
         try:
-            response = await self._handle(request)
+            response = await self._handler(request)
         except ErrorResponse as e:
             response = e.resp
         if isinstance(response, Iterable):
@@ -35,25 +37,6 @@ class AbstractServer(ABC):
     def serve_forever(self):
         """The blocking version of `serve_async`"""
         aiotools.run_async(self.serve_async())
-
-
-class Transport(ABC):
-    @abstractmethod
-    async def listen(self) -> AsyncIterator[Request]:
-        yield
-
-    @abstractmethod
-    async def send(self, request: Request, response: Response):
-        pass
-
-
-class Server(AbstractServer, ABC):
-    def __init__(self, transport: Transport, handler: Handler):
-        self._transport = transport
-        self._handler = handler
-
-    def _handle(self, request: Request) -> Awaitable[TResponse]:
-        return self._handler(request)
 
     def listen(self):
         return self._transport.listen()
